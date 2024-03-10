9333c03a4b4a1c591de6cfb6aea84b4ec42fc55e
Vuk <vukkukulicic57@gmail.com>
2024-03-10 01:13:04 +0100

    Added sit button

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("table", views.table, name="table"),
+    path("error", views.error, name="error")
 ]
--- game/views.py
+++ game/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .util import Player
 
 # Create your views here.
-def index(request):
-    return render(request, "game/index.html")
+def table(request):
+    if request.method == "POST":
+        player = Player()
+        player.set_is_sitting(True)
+
+        return render(request, "game/table.html")
+    print(request.session)
+
+    return render(request, "game/table.html")
+
+def error(request):
+    return HttpResponse("Error")
