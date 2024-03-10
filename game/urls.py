bc3cb5a27d73b99654c031c8d1ea24a1a7642200
Vuk <vukkukulicic57@gmail.com>
2024-03-10 22:33:28 +0100

    Added user functionality

--- game/urls.py
+++ game/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
-    path("table", views.table, name="table"),
-    path("error", views.error, name="error")
+    path('', RedirectView.as_view(url="login")),
+    path("login/", views.login_page, name="login"),
+    path("logout/", views.logout_page, name="logout"),
+    path('register/', views.register, name='register'),
+    path("tables/<str:name>", views.tables, name="tables"),
+    path("table/", views.table, name="table"),
+    path("error/", views.error, name="error")
 ]
--- game/views.py
+++ game/views.py
@@ -1,9 +1,58 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 from .util import Player
 
 # Create your views here.
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username)
+        print(password)
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('tables', kwargs={'name': username}))
+        else:
+            return render(request, "registration/login.html")
+            #TODO
+
+    return render(request, "registration/login.html")
+
+
+def logout_page(request):
+    print(request.user)
+    logout(request)
+    
+    messages.success(request, "You have been logged out successfully.")
+    return redirect('login')
+    #TODO
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def tables(request, name):
+    return render(request, "game/tables.html", {
+        "name": name
+    })
+
+
 def table(request):
     if request.method == "POST":
         player = Player()
@@ -13,6 +62,7 @@ def table(request):
     print(request.session)
 
     return render(request, "game/table.html")
+
 
 def error(request):
     return HttpResponse("Error")
