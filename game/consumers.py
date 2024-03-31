4f359f1ad6b78855b6c3fd7c394a79e2725fe59f
Vuk <vukkukulicic57@gmail.com>
2024-03-31 23:01:42 +0200

    Channels

--- game/consumers.py
+++ game/consumers.py
@@ -1,17 +1,28 @@
-import json
-from .models import Table
+import json, re
+from .models import Table, Player
 from channels.generic.websocket import WebsocketConsumer
-from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
 
 class TablePlayer(WebsocketConsumer):
     def connect(self):
         self.user = self.scope['user']
+
+        table_name = self.scope['url_route']['kwargs']['table_name']
+        sanitized_table_name = re.sub(r'\W+', '_', table_name)
+        self.table_group_name = f"table_{sanitized_table_name}"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.table_group_name, self.channel_name
+        )
         self.accept()
 
-        self.table_group_name = "table_%s" % self.user
-        print(self.table_group_name)
+        #tip vodi na funkciju, slicno ka view u url
+        """
+        async_to_sync(self.channel_layer.group_send)(
+            self.table_group_name, {"type": "test", "message": 'kurac'}
+        )
+        """
 
     def disconnect(self, close_code):
         pass
@@ -20,22 +31,35 @@ class TablePlayer(WebsocketConsumer):
         data = json.loads(text_data)
         action = data.get('action')
 
+        #nekad probat u connect
         if action == 'sit':
             table_name = data.get('table_name')
             table = Table.objects.get(name = table_name)
-            if table.players_number < 7:
+            if table.players_number < 8:
                 table.players_number += 1
+                player = Player.objects.create(username=self.user.username)
+                table.players.add(player)
                 table.save()
                 self.send(text_data=json.dumps({
                     'type': 'sit',
                     'message': table.players_number,                
                 }))
 
+        #nekad probat u disconnect
         elif action == 'unsit':
             table_name = data.get('table_name')
             table = Table.objects.get(name = table_name)
             table.players_number -= 1
+            player = Player.objects.get(username=self.user.username)
+            player.delete()
+            #then table players remove is not needed right?
             table.save()
-            self.send(text_data=json.dumps({
-                'message': table.players_number,                
-            }))
+
+    """
+    def test(self, event):
+        message = event['message']
+        print("Test message received:", message)
+        self.send(text_data=json.dumps({
+            "poruka": 'kurac'
+        }))
+    """
--- game/models.py
+++ game/models.py
@@ -3,26 +3,22 @@ from django.contrib import admin
 
 # Create your models here.
 
-class Table(models.Model):
-    name = models.CharField(max_length=50)
-    blinds = models.CharField(max_length=15)
-    players_number = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
-    
-
 class Player(models.Model):
     username = models.CharField(max_length=50)
-    chips = models.IntegerField()
+    chips = models.IntegerField(default=10000)
     cards = models.CharField(max_length=5, blank=True, null=True)
 
     def __str__(self):
         return self.username
 
-class TableInfo(models.Model):
-    table = models.ForeignKey(Table, on_delete=models.CASCADE)
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+class Table(models.Model):
+    name = models.CharField(max_length=50)
+    blinds = models.CharField(max_length=15)
+    players_number = models.IntegerField(default=0)
+    players = models.ManyToManyField(Player, related_name='tables', blank=True)
+
+    def __str__(self):
+        return self.name
 
 class CommunityCards(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
@@ -34,4 +30,19 @@ class CommunityCards(models.Model):
 
 admin.site.register(Table)
 admin.site.register(Player)
-admin.site.register(TableInfo)
+
+# Assuming you have a Table instance called "table_instance"
+# You can add players to the table like this:
+"""
+table_instance.players.add(player1, player2, player3)
+"""
+
+# You can also remove players from the table:
+"""
+table_instance.players.remove(player1)
+"""
+
+# Get all players associated with a table:
+"""
+players_on_table = table_instance.players.all()
+"""
