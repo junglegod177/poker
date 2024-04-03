69331f4781ea620603cfcbfb42057ed43afc3c0a
Vuk <vukkukulicic57@gmail.com>
2024-04-03 18:38:29 +0200

    Joining table

--- game/consumers.py
+++ game/consumers.py
@@ -17,6 +17,14 @@ class TablePlayer(WebsocketConsumer):
         )
         self.accept()
 
+        #TODO ako je sto pun
+        table = Table.objects.get(name = table_name)
+        if table.players_number < 8:
+            table.players_number += 1
+            player = Player.objects.create(username=self.user.username)
+            table.players.add(player)
+            table.save()
+
         #tip vodi na funkciju, slicno ka view u url
         """
         async_to_sync(self.channel_layer.group_send)(
@@ -25,35 +33,17 @@ class TablePlayer(WebsocketConsumer):
         """
 
     def disconnect(self, close_code):
-        pass
+        table_name = self.scope['url_route']['kwargs']['table_name']
+        table = Table.objects.get(name = table_name)
+        table.players_number -= 1
+        player = Player.objects.get(username=self.user.username)
+        player.delete()
+        table.save()
 
     def receive(self, text_data):
         data = json.loads(text_data)
         action = data.get('action')
 
-        #nekad probat u connect
-        if action == 'sit':
-            table_name = data.get('table_name')
-            table = Table.objects.get(name = table_name)
-            if table.players_number < 8:
-                table.players_number += 1
-                player = Player.objects.create(username=self.user.username)
-                table.players.add(player)
-                table.save()
-                self.send(text_data=json.dumps({
-                    'type': 'sit',
-                    'message': table.players_number,                
-                }))
-
-        #nekad probat u disconnect
-        elif action == 'unsit':
-            table_name = data.get('table_name')
-            table = Table.objects.get(name = table_name)
-            table.players_number -= 1
-            player = Player.objects.get(username=self.user.username)
-            player.delete()
-            #then table players remove is not needed right?
-            table.save()
 
     """
     def test(self, event):
